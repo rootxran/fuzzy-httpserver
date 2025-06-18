cac32f48d1a7af3715714195984c866c5d32f3ae
Pak Cyberbot <93365275+PakCyberbot@users.noreply.github.com>
2025-06-18 13:51:48 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 from setuptools import setup
 
+with open('README.md', 'r',encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='fuzzyhttp',
-    version='0.1.0',
+    name='fuzzy-httpserver',
+    version='1.1.1',
     description='Fuzzy matching HTTP file server with autocomplete and fallback',
     author='PakCyberbot',
     author_email='pakcyberbot@gmail.com',
-    packages=['fuzzyhttp'],
+    url="https://github.com/PakCyberbot/fuzzy-httpserver",  
+    project_urls={  
+        "Source": "https://github.com/PakCyberbot/fuzzy-httpserver",
+        "Bug Tracker": "https://github.com/PakCyberbot/fuzzy-httpserver/issues",
+    },
+    packages=['fuzzy_httpserver'],
     entry_points={
         'console_scripts': [
-            'fuzzyhttp = fuzzyhttp.server:main',
+            'fuzzy-httpserver = fuzzy_httpserver.server:main',
         ],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
     python_requires='>=3.6',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
