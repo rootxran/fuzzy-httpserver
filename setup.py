7d0ed2ce426950301be3a93931b4d0260155e1bf
Pak Cyberbot <93365275+PakCyberbot@users.noreply.github.com>
2025-06-24 18:40:16 +0500

    updated to 1.4.0, improved post data receive and display IP addresses

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r',encoding="utf-8") as fh:
 
 setup(
     name='fuzzy-httpserver',
-    version='1.3.0',
+    version='1.4.0',
     description='Fuzzy matching HTTP file server with autocomplete and fallback',
     author='PakCyberbot',
     author_email='pakcyberbot@pakcyberbot.com',
